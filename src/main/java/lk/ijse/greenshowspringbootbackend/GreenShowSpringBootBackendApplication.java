00fca6564ebcc316de5024438f91cbcd53a83e16
DonShanilka <shanilka799.com>
2024-11-10 23:21:04 -0800

    1st commit

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/GreenShowSpringBootBackendApplication.java
@@ -0,0 +1,12 @@
+package lk.ijse.greenshowspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShowSpringBootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShowSpringBootBackendApplication.class, args);
+    }
+}
