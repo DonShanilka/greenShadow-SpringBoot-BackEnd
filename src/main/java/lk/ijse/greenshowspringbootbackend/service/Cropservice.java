afca42aefed43c3f9165c2f2327a38954aa7e602
DonShanilka <shanilka799.com>
2024-11-13 10:01:12 -0800

    create Cropservice

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/service/Cropservice.java
@@ -0,0 +1,14 @@
+package lk.ijse.greenshowspringbootbackend.service;
+
+import lk.ijse.greenshowspringbootbackend.dto.CropStatus;
+import lk.ijse.greenshowspringbootbackend.dto.impl.CropDTO;
+
+import java.util.List;
+
+public interface Cropservice {
+    void saveCrop(CropDTO cropDTO);
+    List<CropDTO> getAllCrops();
+    CropStatus getCropById(String cropCode);
+    void deleteCropById(String cropCode);
+    void updateCrop(String cropCode,CropDTO cropDTO);
+}
