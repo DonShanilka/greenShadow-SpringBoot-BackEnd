6c887ac1de02c9fca36bc33b5731ade75055f793
DonShanilka <shanilka799.com>
2024-11-13 10:45:00 -0800

    create CropController class in saveCrops method

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/controller/CropController.java
@@ -0,0 +1,34 @@
+package lk.ijse.greenshowspringbootbackend.controller;
+
+import lk.ijse.greenshowspringbootbackend.dto.impl.CropDTO;
+import lk.ijse.greenshowspringbootbackend.exception.DataPersistException;
+import lk.ijse.greenshowspringbootbackend.service.CropService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/crops")
+public class CropController {
+
+    @Autowired
+    private CropService cropService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCrops(@RequestBody CropDTO cropDTO) {
+        try {
+            cropService.saveCrop(cropDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
