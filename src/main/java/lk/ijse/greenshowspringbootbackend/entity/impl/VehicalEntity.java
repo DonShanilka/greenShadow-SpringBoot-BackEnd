22ec2727c4993afd10f95c679c6429dc2859b9e6
DonShanilka <shanilka799.com>
2024-11-12 23:57:42 -0800

    create VehicalEntity

--- src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/VehicalEntity.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/VehicalEntity.java
@@ -1,4 +1,25 @@
 package lk.ijse.greenshowspringbootbackend.entity.impl;
 
+
+    import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "vehicle")
 public class VehicalEntity {
+        @Id
+        private String vehicleCode;
+        private String licensePlateNumber;
+        private String Name;
+        private String category;
+        private String fuelType;
+        private String remark;
+        @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+        @JoinColumn(name = "memberCode")
+        private StaffEntity staff;
 }
