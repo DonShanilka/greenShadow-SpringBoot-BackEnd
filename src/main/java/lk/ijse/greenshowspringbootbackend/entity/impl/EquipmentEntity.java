974d9eb1078f987e4c78e60c77329e1e97879933
DonShanilka <shanilka799.com>
2024-11-12 23:47:22 -0800

    create EquipmentEntity

--- src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/EquipmentEntity.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/EquipmentEntity.java
@@ -1,5 +1,22 @@
 package lk.ijse.greenshowspringbootbackend.entity.impl;
 
-public class EquipmentEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "equipment")
+public class EquipmentEntity {
+    @Id
+    private String equipmentCode;
+    private String name;
+    private String type;
+    private String status;
+    private String availableCount;
 }
