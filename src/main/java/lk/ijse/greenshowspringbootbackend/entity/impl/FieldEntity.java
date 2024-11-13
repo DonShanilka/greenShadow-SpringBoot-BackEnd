17fa0b5bcf01d1d308c0cc9d28e56a04c165fd05
DonShanilka <shanilka799.com>
2024-11-12 23:44:43 -0800

    create FieldEntity

--- src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/FieldEntity.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/FieldEntity.java
@@ -1,5 +1,52 @@
 package lk.ijse.greenshowspringbootbackend.entity.impl;
 
-public class FieldEntity {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.geo.Point;
 
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "field")
+public class FieldEntity {
+    @Id
+    private String fieldCode;
+    private String name;
+    private Point location;
+    private double extentSize;
+    @Column(columnDefinition = "LONGTEXT")
+    private String fieldImage1;
+    @Column(columnDefinition = "LONGTEXT")
+    private String fieldImage2;
+    @JsonIgnore  // Ignore during serialization to avoid recursion
+    @ManyToMany(mappedBy = "fieldList")
+    private List<EquipmentEntity> equipmentsList;
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "field_staff_details",
+            joinColumns = @JoinColumn(name = "fieldCode"),
+            inverseJoinColumns = @JoinColumn(name = "memberCode")
+    )
+    private List<StaffEntity> staffList;
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "field_log_details",
+            joinColumns = @JoinColumn(name = "fieldCode"),
+            inverseJoinColumns = @JoinColumn(name = "logCode")
+    )
+    private List<LogEntity> logList;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "field_crop_details",
+            joinColumns = @JoinColumn(name = "fieldCode"),
+            inverseJoinColumns = @JoinColumn(name = "cropCode")
+    )
+    private List<CropEntity> cropList;
 }
