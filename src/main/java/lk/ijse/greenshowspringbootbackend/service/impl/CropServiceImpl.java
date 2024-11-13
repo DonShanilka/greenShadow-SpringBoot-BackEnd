1827c6f4784edc02049f380af710a71a5dec8e3b
DonShanilka <shanilka799.com>
2024-11-13 10:06:12 -0800

    create CropServiceImpl class

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/service/impl/CropServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.greenshowspringbootbackend.service.impl;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CropServiceImpl {
+
+}
