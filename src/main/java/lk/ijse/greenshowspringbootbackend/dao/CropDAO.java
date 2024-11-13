d9e13633f036b7e0307821cb953879f496c7d5da
DonShanilka <shanilka799.com>
2024-11-13 09:39:20 -0800

    create Mapping class toCropEntity,toCropDTO,cropDTOList and update CropDAO class

--- src/main/java/lk/ijse/greenshowspringbootbackend/dao/CropDAO.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/dao/CropDAO.java
@@ -2,11 +2,8 @@ package lk.ijse.greenshowspringbootbackend.dao;
 
 import lk.ijse.greenshowspringbootbackend.entity.impl.CropEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
-@Repository
+@RestController
 public interface CropDAO extends JpaRepository<CropEntity, String> {
-    @Query(value = "SELECT * FROM crop WHERE crop_code = (SELECT crop_code FROM crop ORDER BY CAST(SUBSTRING(crop_code, 6) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
-    CropEntity findLastRowNative();
 }
--- src/main/java/lk/ijse/greenshowspringbootbackend/util/Mapping.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/util/Mapping.java
@@ -1,4 +1,27 @@
 package lk.ijse.greenshowspringbootbackend.util;
 
+import lk.ijse.greenshowspringbootbackend.dto.impl.CropDTO;
+import lk.ijse.greenshowspringbootbackend.entity.impl.CropEntity;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+//    for crop mapping
+    public CropEntity toCropEntity(CropDTO cropDTO) {
+        return modelMapper.map(cropDTO, CropEntity.class);
+    }
+    public CropDTO toCropDTO(CropEntity cropEntity) {
+        return modelMapper.map(cropEntity, CropDTO.class);
+    }
+    public List<CropDTO> cropDTOList(List<CropEntity> cropEntityList) {
+        return modelMapper.map(cropEntityList, new TypeToken<List<CropDTO>>() {}.getType());
+    }
 }
