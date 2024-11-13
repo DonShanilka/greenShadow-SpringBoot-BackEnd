98668567f50f13f65d521e22ede90e2d1a577d7a
DonShanilka <shanilka799.com>
2024-11-12 23:53:02 -0800

    create UserEntity

--- src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/UserEntity.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/UserEntity.java
@@ -1,4 +1,20 @@
 package lk.ijse.greenshowspringbootbackend.entity.impl;
 
+import jakarta.persistence.*;
+import lk.ijse.greenshowspringbootbackend.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserEntity {
+    @Id
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
