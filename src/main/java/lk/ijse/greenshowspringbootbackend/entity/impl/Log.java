c3454d40e01af9febaee689d5e780cc660147341
DonShanilka <shanilka799.com>
2024-11-15 01:01:23 -0800

    create Log class

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/entity/impl/Log.java
@@ -0,0 +1,23 @@
+package lk.ijse.greenshowspringbootbackend.entity.impl;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "log")
+public class Log {
+    @Id
+    private String logCode;
+    private Date logDate;
+    private String observationDetails;
+    private String observedImage;
+}
