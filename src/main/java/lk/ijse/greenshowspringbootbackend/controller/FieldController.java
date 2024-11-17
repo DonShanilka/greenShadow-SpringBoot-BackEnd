b243ad2c1e39229fa2ebf484f129203cc23e7378
DonShanilka <shanilka799.com>
2024-11-16 23:00:50 -0800

    create updateField FieldController

--- src/main/java/lk/ijse/greenshowspringbootbackend/controller/FieldController.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/controller/FieldController.java
@@ -52,4 +52,34 @@ public class FieldController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateField(
+            @RequestPart("fieldCode") String fieldCode,
+            @RequestPart("fieldName") String fieldName,
+            @RequestPart("location") String location,
+            @RequestPart("extentSize") String extentSize,
+            @RequestPart("fieldImage1") MultipartFile fieldImage1,
+            @RequestPart("fieldImage2") MultipartFile fieldImage2
+    ) {
+
+        try {
+
+            byte[] byteProPic1 = fieldImage1.getBytes();
+            String base64proPic1 = AppUtil.imageBase64(byteProPic1);
+
+            byte[] byteProPic2 = fieldImage2.getBytes();
+            String base64proPic2 = AppUtil.imageBase64(byteProPic2);
+
+            fieldService.saveField(new FieldDTO(fieldCode,fieldName,location,extentSize,base64proPic1,base64proPic2));
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
