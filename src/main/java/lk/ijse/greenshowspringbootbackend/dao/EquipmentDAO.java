259da14e8283ad786d031f6ef293cb32b7aa98b3
DonShanilka <shanilka799.com>
2024-11-18 06:54:37 -0800

    delete dao classes

--- src/main/java/lk/ijse/greenshowspringbootbackend/dao/EquipmentDAO.java
+++
@@ -1,8 +0,0 @@
-package lk.ijse.greenshowspringbootbackend.dao;
-
-import lk.ijse.greenshowspringbootbackend.entity.impl.Equipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipmentDAO extends JpaRepository<Equipment,String> {
-
-}
--- src/main/java/lk/ijse/greenshowspringbootbackend/dao/FieldDAO.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.greenshowspringbootbackend.dao;
-
-import lk.ijse.greenshowspringbootbackend.entity.impl.Field;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FieldDAO extends JpaRepository<Field, String> {
-}
