8cf91558cffa0592ade23a7d0425e0a1825c4a55
DonShanilka <shanilka799.com>
2024-11-18 07:04:49 -0800

    create LogServiceImpl

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/service/impl/LogServiceImpl.java
@@ -0,0 +1,26 @@
+package lk.ijse.greenshowspringbootbackend.service.impl;
+
+import lk.ijse.greenshowspringbootbackend.dto.impl.LogDTO;
+import lk.ijse.greenshowspringbootbackend.service.LogService;
+
+import java.util.List;
+
+public class LogServiceImpl implements LogService {
+
+
+
+    @Override
+    public void saveLog(LogDTO logDTO) {
+
+    }
+
+    @Override
+    public List<LogDTO> getLogs() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteLog(String logCode) {
+
+    }
+}
