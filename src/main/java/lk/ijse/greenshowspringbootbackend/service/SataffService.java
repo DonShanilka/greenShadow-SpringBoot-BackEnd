51c85e4d671adf84a63b5a470c351bf47b0f1c93
DonShanilka <shanilka799.com>
2024-11-19 06:14:53 -0800

    create SataffService

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/service/SataffService.java
@@ -0,0 +1,16 @@
+package lk.ijse.greenshowspringbootbackend.service;
+
+import lk.ijse.greenshowspringbootbackend.dto.impl.FieldStaffDTO;
+import lk.ijse.greenshowspringbootbackend.dto.impl.StaffDTO;
+
+import java.util.List;
+
+public interface SataffService {
+    void saveStaff(StaffDTO staffDTO);
+    void updateStaff(StaffDTO staffDTO);
+    void deleteStaff(String staffCode);
+    List<StaffDTO> getAllStaffs();
+    void saveFieldStaff(FieldStaffDTO fieldStaffDTO);
+    void deleteFieldStaff(String fieldCode,String staffCode);
+//    List<FieldStaffDTO> getAllFieldStaffs();
+}
