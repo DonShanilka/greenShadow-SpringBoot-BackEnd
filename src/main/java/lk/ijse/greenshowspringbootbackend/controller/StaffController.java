f3ebdfc0f9fe6efb3e5619ccfc812460d3c6d82b
DonShanilka <shanilka799.com>
2024-11-20 06:19:35 -0800

    create StaffController in saveFieldStaff method

--- src/main/java/lk/ijse/greenshowspringbootbackend/controller/StaffController.java
+++ src/main/java/lk/ijse/greenshowspringbootbackend/controller/StaffController.java
@@ -1,5 +1,6 @@
 package lk.ijse.greenshowspringbootbackend.controller;
 
+import lk.ijse.greenshowspringbootbackend.dto.impl.FieldStaffDTO;
 import lk.ijse.greenshowspringbootbackend.dto.impl.StaffDTO;
 import lk.ijse.greenshowspringbootbackend.service.StaffService;
 import lk.ijse.greenshowspringbootbackend.util.ResponseUtil;
@@ -36,6 +37,13 @@ public class StaffController {
 
     @GetMapping
     public ResponseUtil getAllStaff() {
-        return new ResponseUtil("Don"," Get All Staff ",staffService.getAllStaffs());
+        return new ResponseUtil("Don"," Get All Staff " ,staffService.getAllStaffs());
     }
+
+    @PostMapping(value = "/fieldstaff", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveFieldStaff(@RequestBody FieldStaffDTO fieldStaffDTO) {
+        staffService.saveFieldStaff(fieldStaffDTO);
+        return new  ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
