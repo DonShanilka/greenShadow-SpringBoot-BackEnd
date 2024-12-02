524ecf2aee4e6b335f908cf927a4e6e2d9205ea6
DonShanilka <shanilka799.com>
2024-12-02 17:20:37 +0530

    create LogFieldDTO

---
+++ src/main/java/lk/ijse/greenshowspringbootbackend/dto/impl/LogFieldDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshowspringbootbackend.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LogFieldDTO {
+    private String logId;
+    private String fieldId;
+}
